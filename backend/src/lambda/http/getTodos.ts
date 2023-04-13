62a506ca2ba9c379c6896b18beebf54473cfeee4
Nikita0000000 <krasnytskyinikita@gmail.com>
2023-04-13 02:56:36 +0100

    added code lamabda function to get all todos

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -7,16 +7,26 @@ import { cors } from 'middy/middlewares'
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
 
-// TODO: Get all TODO items for a current user
+// Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+    const uid = getUserId(event)
+    const todos = await getTodosForUser(uid)
 
-    return undefined
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  }
+)
+
 
 handler.use(
   cors({
-    credentials: true
+    credentials: true,
+    headers: true,
+    origin: '*'
   })
 )
