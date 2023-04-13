058b5a37256e8ba4ec14188807832cc5e9f6dda7
Nikita0000000 <krasnytskyinikita@gmail.com>
2023-04-13 02:54:41 +0100

    added code for generating presigned url

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -2,5 +2,22 @@ import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
 
 const XAWS = AWSXRay.captureAWS(AWS)
+const signVersion = new XAWS.S3({signatureVersion: 'v4'})
 
-// TODO: Implement the fileStogare logic
+// FileStogare logic
+export class AttachmentUtils {
+    constructor(
+        private readonly buckerName:string = process.env.ATTACHMENT_S3_BUCKET,
+        private readonly expireAt:string = process.env.SIGNED_URL_EXPIRATION
+    ){}
+        async generateUploadUrl(uid:string, tid:string): Promise<string>{
+            const attachmentUrl:string = await signVersion.getSignedUrl({
+                Bucket: this.buckerName,
+                Key: `image-${uid}-${tid}`,
+                Expires: this.expireAt
+            })
+
+            return attachmentUrl
+        }
+    
+}
